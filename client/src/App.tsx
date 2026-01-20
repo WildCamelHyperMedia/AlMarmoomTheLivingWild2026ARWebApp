b6a879cf0a3e91e962fe41f1ff13068beb93d049
maffan6 <50048055-maffan6@users.noreply.replit.com>
2026-01-20 18:58:05 +0000

    Restored to '50e36815f5f32505d129445352e8b7d122d5bbe6'
    Replit-Restored-To: 50e36815f5f32505d129445352e8b7d122d5bbe6

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,6 @@ import GalleryPage from "@/pages/GalleryPage";
 import SignUpPage from "@/pages/SignUpPage";
 import IntroPage from "@/pages/IntroPage";
 import AnimalDetailPage from "@/pages/AnimalDetailPage";
-import World3DPage from "@/pages/World3DPage";
 import { LanguageProvider } from "@/lib/language";
 import { UserProvider } from "@/lib/user";
 import { ProgressProvider } from "@/lib/progress";
@@ -22,7 +21,6 @@ function Router() {
       <Route path="/intro" component={IntroPage} />
       <Route path="/gallery" component={GalleryPage} />
       <Route path="/animal/:id" component={AnimalDetailPage} />
-      <Route path="/world3d" component={World3DPage} />
       <Route component={NotFound} />
     </Switch>
   );
